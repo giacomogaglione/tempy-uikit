49a478341221d99bfd764a472d672eabf76bf804
giacomogaglione <gaglione.giacomo@gmail.com>
2021-03-14 04:31:55 +0100

    init

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -10,9 +10,13 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 409 90" {...props}>
-      <image width="409" height="90" href={isDark ? 'images/soju/LogoTextNewDark.png' : 'images/soju/LogoTextNewWhite.png'}/>
+      <image
+        width="409"
+        height="90"
+        href={isDark ? "images/soju/LogoTextNewDark.png" : "images/soju/LogoTextNewWhite.png"}
+      />
     </Svg>
-  )
+  );
 };
 
 export default Logo;
